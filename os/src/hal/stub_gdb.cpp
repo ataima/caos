32c01ff1b0cfef5f4f9230c2f20ebfcea0ca94ec
Angelo Coppi <angelogkcop@hotmail.com>
2017-02-10 17:05:07 +0100

    add stub to debug  with gdb

---
+++ os/src/hal/stub_gdb.cpp
@@ -0,0 +1,32 @@
+////////////////////////////////////////////////////////////////////////////////
+//    Copyright (C) 2016  Angelo Coppi (angelogkcop at hotmail.com )
+//
+//    This program is free software: you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation, either version 3 of the License
+//
+//    This program is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+////////////////////////////////////////////////////////////////////////////////
+// Author : Angelo Coppi (coppi dot angelo at virgilio dot it )
+// History:        
+////////////////////////////////////////////////////////////////////////////////
+
+#include "config.h"
+#include "caos_c_types.h"
+#include "memaux.h"
+
+// stub for memcpy & more missing function . gcc with -O0 and -ggdb reuire memcpy 
+// in ctor and virtual pure default functions...
+
+
+extern "C" int memcpy(s8 *dest,s8 *src,s_t len){
+ return caMemAux::MemCpy(dest,src,len);
+}
+
+
