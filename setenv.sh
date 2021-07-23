6ee6c94c5b6150e5ae4efc8bff9625b2dc4a1769
Angelo Coppi <angelogkcop@hotmail.com>
2021-07-23 17:06:27 +0200

    aggiunto supporto openocd v0.11.rc2 : sudo make jtag -> gdb

--- setenv.sh
+++ setenv.sh
@@ -5,4 +5,4 @@ NAME=$(cat ./gcc-cross/conf.mk | grep TARGET:= | sed 's/:=/ /g'| awk '{print $2}
 
 CROSSGCC=$PWD/$NAME
 echo "CURRENT CROSS PLATFORM : $NAME"
-export PATH=$CROSSGCC/bin:$CROSSGCC/$NAME/bin:/usr/bin:/bin
+export PATH=$CROSSGCC/bin:$CROSSGCC/$NAME/bin:/usr/bin:/bin:/usr/local/bin
